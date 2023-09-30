5b592f02131ac74dd6bd5654feeb94b7a58bcf9d
Irene <irene.muthomi@student.moringaschool.com>
2023-09-30 12:23:22 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+console.log('js integrated')
 function addingEventListener() {
+    const input = document.getElementById('input');
+
+function clickAlert() {
+  alert('I was clicked!');
+}
+
+input.addEventListener('click', clickAlert);
+
 }
